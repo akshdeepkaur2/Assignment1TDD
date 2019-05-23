7bd57d154d36240e78bd4f72c25ef5c04bce7f9a
akshdeepkaur2 <39845021+akshdeepkaur2@users.noreply.github.com>
2019-05-23 09:41:13 -0400

    R2- green

---
+++ Yelling.java
@@ -0,0 +1,16 @@
+
+public class Yelling {
+
+
+public String Scream( String name) {
+	
+	if (name == "Peter") {
+		return "Peter is yelling";
+	}
+	
+	else if ( name == "Null") {
+		return "No one is Yelling";
+	}
+	return name;
+}
+}

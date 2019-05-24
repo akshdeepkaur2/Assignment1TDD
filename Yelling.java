e38fbff14de2105485d491b8e3f96da8894e2d4e
pardeep0407 <39106963+pardeep0407@users.noreply.github.com>
2019-05-24 18:07:09 -0400

    R5- GREEN

--- Yelling.java
+++ Yelling.java
@@ -1,9 +1,17 @@
 public class Yelling 
 {
+	
  public String Scream (String name)
  {
- 
-   if(name == "Peter")
+	 String[] peoplenum;
+	 
+     peoplenum = name.split(",");
+     if(peoplenum.length > 2)
+     {
+    	String allpersons = peoplenum[0]+","+peoplenum[1]+", "+  "and" +peoplenum[2];
+    	return allpersons +" are yelling";
+     }
+     else if(name == "Peter")
   {
  return "Peter is yelling";
  }

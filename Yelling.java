fe8f0737353cd03c770752c7fe6a7ac0bd085e33
pardeep0407 <39106963+pardeep0407@users.noreply.github.com>
2019-05-24 17:32:17 -0400

    R5  - RED

--- Yelling.java
+++ Yelling.java
@@ -1,35 +1,21 @@
 public class Yelling 
-
 {
-
-	public String Scream (String name)
-
-	{
- if(name == "Peter")
-		{
-
- return "Peter is yelling";
-
-	}
-
-	else if(name == "null")
-
-		{
-
-	return "Nobody is yelling";
-
-	}
+ public String Scream (String name)
+ {
  
- if(name == "PETER")
-	{
-String Str = "Peter is yelling";
-System.out.println(Str.toUpperCase());  
-return "PETER IS YELLING";
-
+   if(name == "Peter")
+  {
+ return "Peter is yelling";
+ }
+  else if(name == "")
+  {
+ return "Nobody is yelling";
+ }
+  else if(name == "PETER")
+  {
+ return "PETER IS YELLING";
+ }
+ return name;
+ 
 }
-	
-   return name;	
-
-	}
 }
-

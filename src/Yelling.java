502211cf87a14ace77cc87c4b1ce7ac3ec4eb467
Pardeep <Pardeep@192.168.2.236>
2019-05-24 17:18:46 -0400

    R5--RED

---
+++ src/Yelling.java
@@ -0,0 +1,21 @@
+public class Yelling 
+{
+ public String Scream (String name)
+ {
+ 
+   if(name == "Peter")
+  {
+ return "Peter is yelling";
+ }
+  else if(name == "")
+  {
+ return "Nobody is yelling";
+ }
+  else if(name == "PETER")
+  {
+ return "PETER IS YELLING";
+ }
+ return name;
+ 
+}
+}
---
+++ test/YellingTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+public class YellingTest {
+@Before
+public void setUp() throws Exception {
+}
+@After
+public void tearDown() throws Exception {
+}
+@Test
+public void test1PersonIsYelling() {
+// 1 PERSON IS YELLINg
+Yelling y = new Yelling();
+String result = y.Scream("Peter");
+assertEquals("Peter is yelling",result );
+}
+@Test
+public void test0PersonIsYelling() {
+// NOone PERSON IS YELLINg
+Yelling y = new Yelling();
+String result = y.Scream("");
+assertEquals("Nobody is yelling",result );
+}
+@Test
+public void testUppercaseYelling() {
+// Uppercase  YELLINg
+Yelling y = new Yelling();
+String result = y.Scream("PETER");
+assertEquals("PETER IS YELLING",result );
+}
+@Test
+public void testMoreThanTwoPeopleYelling() {
+// more than 2 people are yelling
+Yelling y = new Yelling(); 
+String result = y.Scream("Peter, Kadeem, Albert,");
+assertEquals("Peter, Kadeem,and Albert are yelling",result );
+}
+}

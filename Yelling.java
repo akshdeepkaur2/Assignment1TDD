21d0db46278162f5c52ca83b325a89e8da34ace2
akshdeepkaur2 <39845021+akshdeepkaur2@users.noreply.github.com>
2019-05-24 22:36:26 -0400

    R4- green

--- Yelling.java
+++ Yelling.java
@@ -1,27 +1,38 @@
 
 public class Yelling {
 	
+	public String Scream(String name) {
 		
-
-public String Scream (String name) {
-	
-	if (name == "Peter") {
-		return "Peter is yelling";
-	}
-	
-	else if ( name == "Null") {
-		return "No one is Yelling";
-	}
-	
-	else if(name == "Peter , Albert") {
 		
-		return " Peter and Albert are yelling";
+		String[] namelist;
+		namelist = name.split(",");
+		if(namelist.length ==2) {
+			String allnames = namelist[0] + " and" + namelist[1];
+			return allnames + "are yelling.";
+			
+			
+			if(namelist.length>2) {
+				String allnames = namelist[0]+","+namelist[1]+"," + "and"+ namelist[2];	
+				return allnames+ " are yelling.";
+				}
+			else {
+				if(name == "")
+				{
+					return "No body is yelling";
+					
+				}
+				else if(name == "PETER") {
+					return "PETER IS YELLING";
+				}
+			}
+			
+			return "Peter is yelling";
+		}
+		
+		
+		
+		
+		
 	}
-	return name;
-}
-
-public String Scream(String[] names) {
-	// TODO Auto-generated method stub
-	return null;
-}
-}
+	
+	}
